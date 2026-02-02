1f704b782c439166a755b8d6c23f6b5c04fbe4e2
vasu777-dotcom <vasubabuparise8@gmail.com>
2026-02-02 15:49:32 +0530

    Spring Security with JWT

---
+++ src/test/java/com/example/SecurityPractiseWithJWT/SecurityPractiseWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurityPractiseWithJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPractiseWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
